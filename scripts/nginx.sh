54d8882f3070bbc20dd342c158cd47d20b67315c
Purushotham0324 <117827825+Purushotham0324@users.noreply.github.com>
2022-12-20 15:31:46 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -rf /var/jenkins_home/workspace/django_ci_cd-Project/scripts/app.conf /etc/nginx/sites-available/
-chmod 710  /var/jenkins_home/workspace/django_ci_cd-Project/app
+chmod 710 /var/jenkins_home/workspace/django_ci_cd-Project/app
 
 ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 nginx -t
