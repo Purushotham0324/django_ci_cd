3e5e5b567b92fd911b488b2691b7ad06e6b9e8b9
Purushotham0324 <117827825+Purushotham0324@users.noreply.github.com>
2022-12-20 13:13:48 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -10,7 +10,7 @@ python3 manage.py collectstatic -- no-input
 
 echo "Migrations done"
 
-cd /var/jenkins_home/workspace/django_ci_cd-Project/
+cd /var/jenkins_home/workspace/django_ci_cd-Project/scripts/
 
 cp -rf gunicorn.socket /etc/systemd/system/
 cp -rf gunicorn.service /etc/systemd/system/
