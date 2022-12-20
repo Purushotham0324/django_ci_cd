e7c01602530a8797c1c1c80786ff2fa05a988866
Purushotham0324 <117827825+Purushotham0324@users.noreply.github.com>
2022-12-20 15:15:25 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 sudo cp -rf app.conf /etc/nginx/sites-available/app
-chmod 710 /var/lib/jenkins/workspace/django-cicd
+chmod 710  /var/jenkins_home/workspace/django_ci_cd-Project/app
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
-sudo nginx -t
+ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+nginx -t
 
-sudo systemctl start nginx
-sudo systemctl enable nginx
+systemctl start nginx
+systemctl enable nginx
 
 echo "Nginx has been started"
 
-sudo systemctl status nginx
+systemctl status nginx
